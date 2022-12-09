a7cf3497a1196dbb0e8587f37b18e966dc24a23a
lfer9980 <luis_fernandez99@hotmail.com>
2022-12-09 11:24:34 -0600

    :art: HashBrowser

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { HashRouter, Routes, Route } from 'react-router-dom'
+import { Menu } from './Menu'
+import { HomePage } from './HomePage'
+import { BlogPage } from './BlogPage'
+import { ProfilePage } from './ProfilePage'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    <>
+      <HashRouter>
+        <Menu />
+
+        <Routes>
+          <Route path='/' element={<HomePage />}/>
+          <Route path='/blog' element={<BlogPage />}/>
+          <Route path='/profile' element={<ProfilePage />}/>
+          <Route path='*' element={<p>Not Found</p>}/> {/* siempre de ultimas */}
+        </Routes>
+      </HashRouter>
+    </>
   )
 }
 
---
+++ src/HomePage.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function HomePage() {
+	return (
+		<p>Homepage</p>
+	);
+}
+
+export {HomePage} ;
